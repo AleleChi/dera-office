94bfc95c2cd952e88476a0731d0fab792cec9021
AleleChi <40716053+AleleChi@users.noreply.github.com>
2026-05-28 18:50:52 +0100

    stull bug fix

--- app/web/src/pages/landing-page/components/Navigation.tsx
+++ app/web/src/pages/landing-page/components/Navigation.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from 'react-router-dom'
 import { motion } from 'framer-motion'
 import { useSelector } from 'react-redux'
-import { RootState } from '../../app/store/store'
+import { RootState } from '../../../app/store/store'
 
 const navItems = [
   { label: 'Features', href: '#features' },
